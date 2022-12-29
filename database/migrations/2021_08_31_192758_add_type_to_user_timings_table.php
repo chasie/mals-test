b3b35f9faff4163e45fee314eb5be0db6d596081
Shift <shift@laravelshift.com>
2022-12-29 22:08:07 +0000

    Adopt anonymous migrations

--- database/migrations/2021_08_31_192758_add_type_to_user_timings_table.php
+++ database/migrations/2021_08_31_192758_add_type_to_user_timings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddTypeToUserTimingsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -37,4 +37,4 @@ class AddTypeToUserTimingsTable extends Migration
             $table->timestamp('time_fix')->nullable();
         });
     }
-}
+};
